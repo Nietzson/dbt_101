94c60bba1b8936b9ca6c90538430cd0b83814f3a
yoann.vigouroux@fondation-fondamental.org <yoann.vigouroux@fondation-fondamental.org>
2024-09-17 08:20:26 +0000

    jaffle modules for orders and customers and model order_customers

--- models/example/sandbox.sql
+++
@@ -1,2 +0,0 @@
-SELECT *
-FROM `dbt-tutorial`.jaffle_shop.customers as c
---
+++ models/example/stg_orders.sql
@@ -0,0 +1,6 @@
+select id as order_id,
+user_id,
+order_date,
+status,
+_etl_loaded_at
+from `dbt-tutorial`.jaffle_shop.orders
